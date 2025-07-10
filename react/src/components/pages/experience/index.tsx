d4e3b839a3a70637a5ea57fbb8afb73fb37711d9
EST0S <tightman8888@gmail.com>
2025-07-09 22:17:43 -0400

    Progress on home page. Main issues: need to fix for mobile, have to add images on right side

--- react/src/components/pages/experience/index.tsx
+++ react/src/components/pages/experience/index.tsx
@@ -4,7 +4,12 @@ export default function Experience() {
 
   return (
     <div className={styles.root}>
-      Testing
+      He has attained the following degrees:
+        <ul>
+          <li>BCS (Bachelor's in Computer Science) Secure Computing, Virginia Tech '23</li>
+          <li>B.S. Applied Discrete Mathematics, Virginia Tech '23</li>
+          <li>M.S. Mathematics, Virginia Tech '25</li>
+        </ul>
     </div>
   )
 }
--- react/src/components/pages/home/index.tsx
+++ react/src/components/pages/home/index.tsx
@@ -4,10 +4,28 @@ export default function Home() {
 
   return (
     <div className={styles.root}>
-      <div className={styles.home}>
-        test
+      <div className={styles.section}>
+        <div className={styles.home}>
+          <h3 className={styles.profession}>
+            Software Developer
+          </h3>
+          <h1 className={styles.name}>
+            Evan Stosic
+          </h1>
+          <p>
+            Hello, my name is Evan Stosic. I am currently a Software Developer 
+            at <a href="https://www.tditek.com/" target="_blank">TDI Technologies Inc.</a> and <a href="https://littech.ai/" target="_blank">LitTech Inc.</a> I 
+            specialize in full-stack development, primarily on web applications built with React and Next.js.
+            I also have experience in AI development, data engineering (Microsoft stack), and secure software development.
+          </p>
+          <p>
+            Feel free to connect with me on LinkedIn, GitHub, or through email by clicking the icons on the header
+            of this site. Also, you're welcome to explore this site as I update it with new work experience, projects,
+            and blog entries.
+          </p>
+        </div>
       </div>
-      <div className={styles.collage}>
+      <div className={styles.section}>
         test
       </div>
     </div>
